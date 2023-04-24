5ea9416ac5a72aa080fc5001879f1568106d901d
David Lai <pf.davidlai@gmail.com>
2023-04-24 03:37:46 -0700

    started nav skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignUpFormPage';
-
+import Navigation from "./components/Navigation";
 
 function App() {
 
   return (
     <>
       <h1>Hello from App</h1>
+      <Navigation />
       <Switch>
       <Route path="/login">
         <LoginFormPage />
---
+++ frontend/src/components/Navigation/searchbar.js
@@ -0,0 +1,14 @@
+
+
+function Searchbar() {
+
+
+    return(
+        <div>
+            <textarea placeholder="Search bar"/>
+        </div>
+    )
+
+}
+
+export default Searchbar;
