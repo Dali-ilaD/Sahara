f280843c66003958b4f726a64c66b4be30318347
David Lai <pf.davidlai@gmail.com>
2023-05-30 13:40:04 -0700

    finish model, controller, and migration

--- db/migrate/20230526160315_create_reviews.rb
+++ db/migrate/20230526160315_create_reviews.rb
@@ -1,10 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
-        t.references "user_id", null: false, foreign_key: true
-        t.references "product_id", null: false, foreign_key: true
-        t.text "body", null: false
-        t.integer "rating", null: false
+        t.references :user, null: false, foreign_key: true
+        t.references :product, null: false, foreign_key: true
+        t.text :body, null: false
+        t.integer :rating, null: false
       t.timestamps
     end
   end
