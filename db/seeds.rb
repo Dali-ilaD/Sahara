ae708b089f4c4b76080ec3f3a11bcf0627391462
David Lai <pf.davidlai@gmail.com>
2023-08-21 11:41:06 -0700

    seeding users

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,30 +10,31 @@ require "open-uri"
 
 
 ApplicationRecord.transaction do 
+  
   #   puts "Destroying tables..."
   #   # Unnecessary if using `rails db:seed:replant`
-  #   User.destroy_all
+    User.destroy_all
   
   #   puts "Resetting primary keys..."
   #   # For easy testing, so that after seeding, the first `User` has `id` of 1
   #   ApplicationRecord.connection.reset_pk_sequence!('users')
   
   #   puts "Creating users..."
-  #   # Create one user with an easy to remember username, email, and password:
-  #   User.create!(
-  #     username: 'Demo-lition', 
-  #     email: 'demo@user.io', 
-  #     password: 'password'
-  #   )
+    # Create one user with an easy to remember username, email, and password:
+    User.create!(
+      username: 'Demo-lition', 
+      email: 'demo@user.io', 
+      password: 'password'
+    )
   
-  #   # More users
-  #   5.times do 
-  #     User.create!({
-  #       username: Faker::Internet.unique.username(specifier: 3..9),
-  #       email: Faker::Internet.unique.email,
-  #       password: Faker::Internet.password(min_length: 6, max_length: 30)
-  #     }) 
-  #   end
+    # More users
+    5.times do 
+      User.create!({
+        username: Faker::Internet.unique.username(specifier: 3..9),
+        email: Faker::Internet.unique.email,
+        password: Faker::Internet.password(min_length: 6, max_length: 30)
+      }) 
+    end
   
   #   products =
   #   3.times do
